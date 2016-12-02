657e7d96674c06e59e65c476ba74f149bf688325
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-12-01 16:52:56 -0800

    [RHA-2] Updated the pki and rvi modules with the latest code

--- app/src/main/java/com/jaguarlandrover/hvacdemo/HVACManager.java
+++ app/src/main/java/com/jaguarlandrover/hvacdemo/HVACManager.java
@@ -18,11 +18,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 import com.google.gson.internal.LinkedTreeMap;
-import com.jaguarlandrover.pki.PKICertificateResponse;
-import com.jaguarlandrover.pki.PKICertificateSigningRequestRequest;
-import com.jaguarlandrover.pki.PKIManager;
-import com.jaguarlandrover.pki.PKIServerResponse;
-import com.jaguarlandrover.rvi.*;
+import org.genivi.pki.PSCertificateResponse;
+import org.genivi.pki.PSCertificateSigningRequestRequest;
+import org.genivi.pki.PKIManager;
+import org.genivi.pki.ProvisioningServerResponse;
+import org.genivi.pki.ProvisioningServerResponse;
+import org.genivi.rvi.*;
 
 import java.security.KeyStore;
 import java.util.*;
@@ -41,7 +42,7 @@ public class HVACManager
     private final static String PROVISIONING_SERVER_BASE_URL = "http://38.129.64.40:8000";
     private final static String PROVISIONING_SERVER_CSR_URL  = "/csr_veh";
 
-    private static ArrayList<String> SIGNED_CREDENTIALS =
+    private static ArrayList<String> SIGNED_PRIVILEGES =
             new ArrayList<>(Arrays.asList(
                     "eyJhbGciOiAiUlMyNTYiLCJ0eXAiOiAiSldUIn0=.eyJpc3MiOiAiR0VOSVZJIiwgInJpZ2h0X3RvX2ludm9rZSI6IFsiZ2VuaXZpLm9yZy92ZWhpY2xlLysvaHZhYy8rIl0sICJyaWdodF90b19yZWNlaXZlIjogWyJnZW5pdmkub3JnL2FuZHJvaWQvKy9odmFjLysiXSwgInZhbGlkaXR5IjogeyAic3RhcnQiOiAxNDUyNDcwNDAwLCAic3RvcCI6IDE0ODQwOTI4MDB9LCAiZGV2aWNlX2NlcnQiOiAiTUlJRVpEQ0NBa3dDQmdGWWpvZWRVakFOQmdrcWhraUc5dzBCQVFzRkFEQ0JrekVMTUFrR0ExVUVCaE1DVlZNeER6QU5CZ05WQkFnTUJrOXlaV2R2YmpFUk1BOEdBMVVFQnd3SVVHOXlkR3hoYm1ReER6QU5CZ05WQkFvTUJrZEZUa2xXU1RFTU1Bb0dBMVVFQ3d3RFVsWkpNUmN3RlFZRFZRUUREQTR4T1RJdU1UWTRMakUyTGpJME5URW9NQ1lHQ1NxR1NJYjNEUUVKQVJZWmNHUjRiM04wWXk1aGJtUnliMmxrUUdkdFlXbHNMbU52YlRBZUZ3MHhOakV4TWpNd01ERXpNakphRncweE56RXhNak13TURFek1qSmFNRmN4SnpBbEJnTlZCQU1NSG1GdVpISnZhV1F2VEUxSmMwUk5iVk5SVDJGcWVGZGhVbWh1UlhCMVFURVBNQTBHQTFVRUNnd0dSMlZ1YVhacE1Sc3dHUVlEVlFRTERCSkJibVJ5YjJsa0lGVnViRzlqYXlCQmNIQXdnZ0VpTUEwR0NTcUdTSWIzRFFFQkFRVUFBNElCRHdBd2dnRUtBb0lCQVFERlJFY0kyUEpGYXJGLzRwYnNvd3NrWVB5L0NFWDBZTHRFVHE0QW5XTzM3K05PTU1YTlJYUW5uV25vaGRZaERGZ3dFU0tOenVPeFU3QjNQNFZhY1Nub2I5U3dCbitRUE9PTHVDcGI1dCs1NmE2THlIWHNFT1JyRjNjSlFZczU4SzdpQ3dMZ3c5OXpjRFpzSFgzVWJaUXI2RHdLTUQvbE9Jb1JLRW94UTZqSCtMcXRHTkdIOWxrZXpBd2paYWlteVMwTXRNUit3ZG5nQktBVXJtcnEwYzFYSFpwK1hXSXpxb0hzT0ZjejQvVlJaM25nczNPenE0VzR1MkNTa25nekNEOW9DdHlCK1JLL20rU1NxbFlZSFNrMXFRaW92OWYzQ3laRkFUZDVqVEhyZHFYdmtIUVMvOEsxUEdOVkdnMGxZdlhxaTJqVjFTNXZsRVhkaXlWODhRaUJBZ01CQUFFd0RRWUpLb1pJaHZjTkFRRUxCUUFEZ2dJQkFGWVF4V0dLTmZQNFBYNUM2UDF6R3hkMTBGQWN3SmtTYWhzaGw5SytDcS9iMFhtTE9SbCt1OFlsVWcyWWwvUGl2dEtxZ3c0ZWhRZlo5OUYyZ2lNWmltejVmejNxT3IyTzJXckswSHh3V2FYSE9ldnYzZEE1cWtUUlBMZ1JpYkJGQmFCRE9PWFlhbHc4bzZUT0ZPWXR3NStkblFRNm9Qa3QwYlVyMElGb1FUeXJOMXVmUlkwTlNFdDJ0UEMrS2lNT2J0cSthUXYydC9oNWxPSm5uWUN4cklYay9GZ0c4a1VOTEdzNkdlQ3ZvYlhnRlVaUzluQ1JEQUpYeFBiWWRNbXdVeVB0NVl1VjFza25hdDhsUHd5K2RSeVRnb012SC9YQjRUQ1pFUjZZakZvb3NOTnJjYlNXMFhXZHFwSFdrRU04ZEtabHdMeHhyY1lnS2hwMVZpQ3VVTi9qSVBDQk83aEZUWTlOM1FXcUt3Z0ZteHhPMC81RWJSejVJK3lJeVNaRWZxcFBNL2FYRTNiTXBPaWFRQlpvSGMxRGNvcE9uc25sTmxDdmJLVWJENHlJSUIrWDJvcEM2VHBGTXNIcGl5QnJYV3J5YjBzTEFNT3NtdXhDalhTNGpma29YZ3hERjNYMWFYR3pWR0I4ZkZEdHU4V2FRUkRDVXNZc0I3ODBxeUdlZldXaHNnNHlkQ0pmczFkSHVDaTZTOWl1VXdzMXdna0t3azZma2VNWDFoZkJoaDJCVy9yL045ZDhLN3E4TE1JMGh1djVEMC9FRHMrR0hBNXozY1pjS1VKN3BCTFA2WS85RVdPZmVWMjB4cCsyN0k3b2E3UnJHalBaUitONjNNT3lsQ1hCZFhucmtnZXphSUVsNVJvVzlKSjB4SkRBY3l5R085c2QvdVdyQVk4VCIsICJjcmVhdGVfdGltZXN0YW1wIjogMTQ3NjEzOTg3OCwgImlkIjogImY5MTIzOGJjLTUwZDQtNDEwZC1hOTIzLWEwOGUyOTZjNGVhMyJ9.PIwnFP8Kx-u3cHA4CrHxISH0lCr_b0Neq1Om06IquZKjw9AwSKGN8AusKJXOBTAWny-61QOkQrUQ_qXIInkhIRNl6A1sqTRH4i6M_kfn3BiLS8f_xlGxVjJp_lGeQdgfapgNl2cESRc7YuJzeVEymhc3vu3zzfuuOXc539ywSVaO6nt4dhwNcRHyT-aaredoRfw7JDCf8m8SYvmQOb35kxVY3VjleUG0SaoMG-x4MwabABMZjX56CijiWtWbORw83aDAOqU78M8xmOqvFQycBZI5ZQcOlTdOosW2P0kro2vCHdvFDxsHS0HwMKkBtQqk_K2kGcme47a_nuaHiFX-WzQZBZWwi_TKlZKnCDAACxX1ZZ0aE4CTysalh4hdLDApS3tMicrRNXi1f-Nt9HUgvKfZPovN0-4o8wuUQ_2qp7hRoU-zgGeK9MGfQ5bHhnkJam-M0kgBkzgnabZoaeikdMmzur4Lbo_xYbqQMTRfgQK3mzQa73sAns0cCj3AOEXOG-Ny14In4_dQQM7F1zpI7UHGYrLU9PvvOTK49wU_MJKwhXmGwEGajJWlti3_qUzx24T7CbQD1lGy-y7QpiU3qus5pUFVQgWqPDGAB5N7dZEJ28pE_RjyUuYN9YidxRsglwxlzCc0pTNf01Td6s433sN34jGQ3q1u1BTv7f3235M="
             ));
@@ -86,7 +87,7 @@ public class HVACManager
     }
 
     private HVACManager() {
-        node = new RVIRemoteNode(applicationContext);
+        node = new RVIRemoteNode();
 
         node.setListener(new RVIRemoteNodeListener()
         {
@@ -221,7 +222,7 @@ public class HVACManager
     }
 
     static void initializeRvi() {
-        RVILocalNode.start(HVACApplication.getContext(), RVI_DOMAIN);
+        RVILocalNode.setRviDomain(RVI_DOMAIN);
 
         if (PKIManager.hasValidDeviceCert(HVACApplication.getContext()) && PKIManager.hasValidServerCert(HVACApplication.getContext())) {
             setUpRviAndConnectToServer(PKIManager.getServerKeyStore(HVACApplication.getContext()), PKIManager.getDeviceKeyStore(HVACApplication.getContext()), null, null);
@@ -250,26 +251,26 @@ public class HVACManager
                 Log.e(TAG, reason.getLocalizedMessage());
             }
 
-        }, start.getTime(), end.getTime(), X509_PRINCIPAL_PATTERN, RVILocalNode.getLocalNodeIdentifier(HVACApplication.getContext()), X509_ORG_UNIT);
+        }, 2048, false, start.getTime(), end.getTime(), X509_PRINCIPAL_PATTERN, RVILocalNode.getLocalNodeIdentifier(HVACApplication.getContext()), X509_ORG_UNIT);
     }
 
     private static void sendCertificateSigningRequest(String certificateSigningRequest) {
-        PKICertificateSigningRequestRequest request = new PKICertificateSigningRequestRequest(certificateSigningRequest);
+        PSCertificateSigningRequestRequest request = new PSCertificateSigningRequestRequest(certificateSigningRequest);
 
         PKIManager.sendCertificateSigningRequest(HVACApplication.getContext(), new PKIManager.ProvisioningServerListener() {
             @Override
-            public void managerDidReceiveResponseFromServer(PKIServerResponse response) {
-                if (response.getStatus() == PKIServerResponse.Status.VERIFICATION_NEEDED) {
+            public void managerDidReceiveResponseFromServer(ProvisioningServerResponse response) {
+                if (response.getStatus() == ProvisioningServerResponse.Status.VERIFICATION_NEEDED) {
                     Log.e(TAG, "Problem: verification needed...");
 
-                } else if (response.getStatus() == PKIServerResponse.Status.CERTIFICATE_RESPONSE) {
-                    Log.d(TAG, "Certificate signing request received and server sent back certs and creds.");
+                } else if (response.getStatus() == ProvisioningServerResponse.Status.CERTIFICATE_RESPONSE) {
+                    Log.d(TAG, "Certificate signing request received and server sent back certs and privileges.");
 
-                    PKICertificateResponse certificateResponse = (PKICertificateResponse) response;
+                    PSCertificateResponse certificateResponse = (PSCertificateResponse) response;
 
-                    setUpRviAndConnectToServer(certificateResponse.getServerKeyStore(), certificateResponse.getDeviceKeyStore(), null, certificateResponse.getJwtCredentials());
+                    setUpRviAndConnectToServer(certificateResponse.getServerKeyStore(), certificateResponse.getDeviceKeyStore(), null, certificateResponse.getJwtPrivileges());
 
-                } else if (response.getStatus() == PKIServerResponse.Status.ERROR) {
+                } else if (response.getStatus() == ProvisioningServerResponse.Status.ERROR) {
                     Log.e(TAG, "Error from server");
 
                 }
@@ -279,18 +280,18 @@ public class HVACManager
 
     }
 
-    private static void setUpRviAndConnectToServer(KeyStore serverCertificateKeyStore, KeyStore deviceCertificateKeyStore, String deviceCertificatePassword, ArrayList<String> newCredentials) {
+    private static void setUpRviAndConnectToServer(KeyStore serverCertificateKeyStore, KeyStore deviceCertificateKeyStore, String deviceCertificatePassword, ArrayList<String> newPrivileges) {
 
         try {
             RVILocalNode.setServerKeyStore(serverCertificateKeyStore);
             RVILocalNode.setDeviceKeyStore(deviceCertificateKeyStore);
             RVILocalNode.setDeviceKeyStorePassword(deviceCertificatePassword);
 
-            if (newCredentials != null)
-                for (String credentials : newCredentials)
-                    SIGNED_CREDENTIALS.add(credentials);
+            if (newPrivileges != null)
+                for (String privileges : newPrivileges)
+                    SIGNED_PRIVILEGES.add(privileges);
 
-            RVILocalNode.setCredentials(HVACApplication.getContext(), SIGNED_CREDENTIALS);
+            RVILocalNode.setPrivileges(HVACApplication.getContext(), SIGNED_PRIVILEGES);
 
             pkiComplete = true;
 
